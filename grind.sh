d68378b825634415378bcc0f5673f787121a3b67
1313ou <1313ou@gmail.com>
2026-05-25 10:21:49 +0200

    grind scripts

---
+++ grind.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2021-2024. Bernard Bou.
+#
+
+set -e
+
+if [ "$*" == "" ]; then
+  echo "
+in1          String,                                         Input dir or file
+in2          String,                                         Extra Input dir or file
+out          String,                                         Output dir or file
+operation    String,  shortName = do, fullName = operation,  Operation                       default=nothing
+outFormat    String,  shortName = of, fullName = out_format, Output format                   default=yaml
+outInfo      String,  shortName = i,  fullName = out_info,   Output info                     default=oewn.info
+outOne       Boolean, shortName = 1,  fullName = out_one,    Output one file                 default=false
+outMerge     Boolean, shortName = m,  fullName = merge,      Do not group generated entries  default=false
+verbose      Boolean, shortName = v,  fullName = verbose,    Verbose output                  default=false
+"
+exit 1
+fi
+
+jar=yam2plus-2.4.0-uber.jar
+if [ ! -e "${jar}" ]; then
+  if [ ! -e "target/${jar}" ]; then
+    echo "Non existing uber jar" >&2
+    exit 1
+    fi
+  ln -s "target/${jar}"
+  fi
+if [ ! -e "${jar}" ]; then
+  echo "Non existing uber jar" >&2
+  exit 2
+  fi
+java -ea -jar "${jar}" "$*"
